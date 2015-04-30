634ecc17f36d7c0a4856dd502d0bbde309473fa3
Alejandro Arbelaez <dragon198658@gmail.com>
2015-04-30 14:48:23 -0500

    create user

--- model/user.js
+++ model/user.js
@@ -17,3 +17,36 @@ exports.userList = function userList (callback) {
         }
     });
 };
+
+exports.createUser = function createUser (name, password, email, contactnumber, callback){
+    var User = mongoose.model('User');
+    var result = null;
+    var newUser = new User({name: name, password: password, email: email, contactnumber: contactnumber});
+
+    User.find({name: name, email: email}, function (err, docs) {
+        if(!err){
+            result = docs[0];
+            console.log("Found something "+ result);
+        }
+        else{
+            console.log("there was an error finding an existing user: " + err);
+            result = -1;
+            throw err;
+        }
+    });
+
+    if( result == null){
+        newUser.save(function(err){
+            if(err) return console.error("Error while saving new User: "+err);
+            console.log("User Saved");
+            //todo callback return user creation successful
+            callback();
+        });
+    }
+    else {
+        console.log("user Already in DB");
+        //todo callback return user already in db
+        callback();
+    }
+
+};
